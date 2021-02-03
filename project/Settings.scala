a50487dd4aa10aba146e93eeef1b2f3e1bfe5050
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-02-03 13:13:14 +0100

    Update kind-projector to 0.11.3 (#33)
    * Update kind-projector to 0.11.3
    
    * Update kind-projector to 0.11.3
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- project/Settings.scala
+++ project/Settings.scala
@@ -7,7 +7,7 @@ object Settings {
   private def crossPlugin(x: sbt.librarymanagement.ModuleID) = compilerPlugin(x.cross(CrossVersion.full))
 
   val compilerPlugins: Seq[sbt.librarymanagement.ModuleID] = Seq(
-    crossPlugin("org.typelevel"    %% "kind-projector"     % "0.11.0"),
+    crossPlugin("org.typelevel"    %% "kind-projector"     % "0.11.3"),
     crossPlugin("com.github.cb372" %% "scala-typed-holes"  % "0.1.7"),
     compilerPlugin("com.olegpy"    %% "better-monadic-for" % "0.3.1")
   )
