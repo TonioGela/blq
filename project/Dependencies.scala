f403c301ca179c6055ea1277cab8cc13e68ebdd1
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-02-17 01:28:15 +0100

    Update munit to 0.7.22 (#41)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,5 +14,5 @@ object Dependencies {
     "org.scala-lang.modules" %% "scala-parallel-collections"  % "1.0.0"
   )
 
-  lazy val testDependencies: Seq[ModuleID] = Seq("org.scalameta" %% "munit" % "0.7.21")
+  lazy val testDependencies: Seq[ModuleID] = Seq("org.scalameta" %% "munit" % "0.7.22")
 }
