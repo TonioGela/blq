6872c10c777d7889be19a56a9f10759d76205b62
Scala Steward <me@scala-steward.org>
2021-03-04 23:18:11 +0100

    Update scala-parallel-collections to 1.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     "org.scodec"             %% "scodec-bits"                 % "1.1.24",
     "com.monovore"           %% "decline"                     % "1.4.0",
     "com.zendesk"             % "mysql-binlog-connector-java" % "0.24.1",
-    "org.scala-lang.modules" %% "scala-parallel-collections"  % "1.0.0"
+    "org.scala-lang.modules" %% "scala-parallel-collections"  % "1.0.1"
   )
 
   lazy val testDependencies: Seq[ModuleID] = Seq("org.scalameta" %% "munit" % "0.7.22")
