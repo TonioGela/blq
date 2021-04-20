5bfa599681b382f840d95fae5688748a678824c3
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-04-20 03:53:57 +0200

    Update cats-core, cats-kernel to 2.6.0 (#63)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,8 @@ import sbt.Keys._
 object Dependencies {
 
   lazy val mainDependencies: Seq[ModuleID] = Seq(
-    "org.typelevel"          %% "cats-core"                   % "2.5.0",
-    "org.typelevel"          %% "cats-kernel"                 % "2.5.0",
+    "org.typelevel"          %% "cats-core"                   % "2.6.0",
+    "org.typelevel"          %% "cats-kernel"                 % "2.6.0",
     "com.chuusai"            %% "shapeless"                   % "2.3.4",
     "org.scodec"             %% "scodec-core"                 % "1.11.7",
     "org.scodec"             %% "scodec-bits"                 % "1.1.25",
