f5b3799164d654282018a3c28bce0813c085ac54
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-11-30 17:40:09 +0100

    Update scodec-bits to 1.1.30 (#125)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     "org.typelevel"          %% "cats-kernel"                 % "2.7.0",
     "com.chuusai"            %% "shapeless"                   % "2.3.7",
     "org.scodec"             %% "scodec-core"                 % "1.11.9",
-    "org.scodec"             %% "scodec-bits"                 % "1.1.29",
+    "org.scodec"             %% "scodec-bits"                 % "1.1.30",
     "com.monovore"           %% "decline"                     % "2.2.0",
     "com.zendesk"             % "mysql-binlog-connector-java" % "0.25.4",
     "org.scala-lang.modules" %% "scala-parallel-collections"  % "1.0.4"
